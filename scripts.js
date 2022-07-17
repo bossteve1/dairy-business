35847f2f37161bd845aa123fbe293b7342933816
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-07-17 20:35:14 +0300

    create weekly and monthly production

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var prod = document.getElementById('prod')
-var income =document.getElementByI('income')
+var income =document.getElementById('income')
 var monthly = document.getElementById('monthly')
 var report = document.getElementById('reports')
 
@@ -16,4 +16,22 @@ prod.addEventListener('click', function(){
     report.innerHTML += "Your production in Shed C 486 litres per day <br />"
     report.innerHTML += "Your production in Shed D 572 litres per day <br />"
     report.innerHTML += "The total production is 1876 litres per day <br />"
+})
+var selling_price = 45
+var weekly_time = 7
+var monthly_time = 30
+
+// var incomeOverTime = function(selling_price, time){
+//     var total = selling_price*time*1876
+//     return total
+// }
+
+
+income.addEventListener('click', function(){ 
+    var weekly_total = selling_price*weekly_time*1876
+    var monthly_total = selling_price*monthly_time*1876
+
+    // report.textContent = 'Your weekly income will be Ksh ' + weekly_total
+    report.innerHTML = 'Your weekly income is Ksh ' + weekly_total + '<br />'
+    report.innerHTML += 'Your monthly income is Ksh ' + monthly_total + '<br />'
 })
